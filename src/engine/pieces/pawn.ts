7b6a4735264329c557e8911eca28da05fe1f89c9
C0rMon <Colm.Monahan@softwire.com>
2024-07-04 17:10:28 +0100

    Added isMoveValid method in pawns, pawns no longer move into other pieces infront

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -12,16 +12,49 @@ export default class Pawn extends Piece {
         const currentPosition = board.findPiece(this);
         let moveList = [];
         if (this.player === Player.WHITE){
-            moveList.push(Square.at(currentPosition.row+1, currentPosition.col));
-            if (currentPosition.row == 1) {
-                moveList.push(Square.at(currentPosition.row+2, currentPosition.col));
+            const candidateMove1 = Square.at(currentPosition.row+1, currentPosition.col);
+            if (this.isMoveValid(board, candidateMove1)){
+
+                moveList.push(Square.at(currentPosition.row+1, currentPosition.col));
+                
+                if (currentPosition.row === 1) {
+                    const candidateMove2 = Square.at(currentPosition.row+2, currentPosition.col);
+
+                    if (this.isMoveValid(board, candidateMove2)){
+                        moveList.push(Square.at(currentPosition.row+2, currentPosition.col));
+                        
+                    }
+                }
             }
+            
         } else{
-            moveList.push(Square.at(currentPosition.row-1, currentPosition.col));
-            if (currentPosition.row == 6) {
-                moveList.push(Square.at(currentPosition.row-2, currentPosition.col));
+            const candidateMove3 = Square.at(currentPosition.row-1, currentPosition.col);
+            if (this.isMoveValid(board, candidateMove3)){
+
+                moveList.push(Square.at(currentPosition.row-1, currentPosition.col));
+                
+                if (currentPosition.row === 6) {
+                    const candidateMove4 = Square.at(currentPosition.row-2, currentPosition.col);
+
+                    if (this.isMoveValid(board, candidateMove4)){
+                        moveList.push(Square.at(currentPosition.row-2, currentPosition.col));
+                        
+                    }
+                }
             }
         }
         return moveList;
     }
+
+    private isMoveValid(board: Board, newMove: Square){
+        let pieceAtMove : Piece | undefined;
+        pieceAtMove = board.getPiece(newMove);
+        if (pieceAtMove === undefined){
+            return true;
+        } 
+        return false;
+
+    }
+
+
 }
