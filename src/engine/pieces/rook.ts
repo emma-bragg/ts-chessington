4e2bd26ae5e7c5d59cacbad94bc69703799c2968
C0rMon <Colm.Monahan@softwire.com>
2024-07-04 13:44:17 +0100

    Made rooks move laterally

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,27 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition = board.findPiece(this);
+        
+        const moveList = this.lateralMoves(currentPosition);
+
+        return moveList;
+    }
+
+    public lateralMoves(currentPosition: Square){
+        const moveList = [];
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++){
+
+            if (i!=currentPosition.col){
+                moveList.push(Square.at(currentPosition.row, i));
+            }
+            
+            if (i!=currentPosition.row){
+                moveList.push(Square.at(i, currentPosition.col));
+            }
+        }
+        
+        return moveList;
+        
     }
 }
