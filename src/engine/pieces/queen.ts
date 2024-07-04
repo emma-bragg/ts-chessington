32e53391c5c8d4e939ef18f6ef1c1bb9494af19d
sophieclaxton <sophie.claxton@softwire.com>
2024-07-04 14:54:39 +0100

    Red-5 Implementation - queen movement as fake bishop and fake rook

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Bishop from './bishop';
+import Rook from './rook';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,18 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let validMoves = new Array(0)
+        let currentSquare = board.findPiece(this)
+
+        let fakeBishop = new Bishop(this.player)
+        board.setPiece(currentSquare, fakeBishop)
+        validMoves = [...validMoves, ...fakeBishop.getAvailableMoves(board)]
+
+        let fakeRook = new Rook(this.player)
+        board.setPiece(currentSquare, fakeRook)
+        validMoves = [...validMoves, ...fakeRook.getAvailableMoves(board)]
+
+        board.setPiece(currentSquare, this)
+        return validMoves
     }
 }
