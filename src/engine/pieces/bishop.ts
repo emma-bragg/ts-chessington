9c0a2c70d18530b4dac343bed68e6b20a170e34a
GuyDav <guy.david@softwire.com>
2024-07-04 14:37:16 +0100

    impl bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves = [];
+        let currentPosition = board.findPiece(this);
+        let oldRow = currentPosition.row;
+        let oldCol = currentPosition.col;
+        for (let newRow = 0; newRow < 8; newRow++) {
+            for (let newCol = 0; newCol < 8; newCol++) {
+               if ((newRow-newCol == oldRow-oldCol || newRow+newCol == oldRow+oldCol) && (newRow != oldRow || newCol != oldCol)) {
+                availableMoves.push(new Square(newRow, newCol));
+               }
+            }
+        }
+        
+        return availableMoves;
     }
 }
