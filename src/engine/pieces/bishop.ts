348f0360e1a293834ce9282467bbada05676435e
sophieclaxton <sophie.claxton@softwire.com>
2024-07-04 14:48:01 +0100

    Red-4 implementation - Bishops moving diagonally

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,47 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let validMoves = new Array(0)
+        let currentSquare = board.findPiece(this)
+        let row = currentSquare.row
+        let col = currentSquare.col
+        
+        for (let i = 1; (row + i < GameSettings.BOARD_SIZE && col + i < GameSettings.BOARD_SIZE); i++) {
+            let nextSquare = Square.at(row + i, col + i)
+            let moveDiagnostics = this.isValidMove(board, nextSquare)
+            if (moveDiagnostics['isValid'])
+                validMoves.push(nextSquare)
+            if (moveDiagnostics['break'])
+                break
+        }
+
+        for (let i = 1; (row + i < GameSettings.BOARD_SIZE && col - i >= 0); i++) {
+            let nextSquare = Square.at(row + i, col - i)
+            let moveDiagnostics = this.isValidMove(board, nextSquare)
+            if (moveDiagnostics['isValid'])
+                validMoves.push(nextSquare)
+            if (moveDiagnostics['break'])
+                break
+        }
+
+        for (let i = 1; (row - i >= 0 && col + i < GameSettings.BOARD_SIZE); i++) {
+            let nextSquare = Square.at(row - i, col + i)
+            let moveDiagnostics = this.isValidMove(board, nextSquare)
+            if (moveDiagnostics['isValid'])
+                validMoves.push(nextSquare)
+            if (moveDiagnostics['break'])
+                break
+        }
+
+        for (let i = 1; (row - i >= 0 && col - i >= 0); i++) {
+            let nextSquare = Square.at(row - i, col - i)
+            let moveDiagnostics = this.isValidMove(board, nextSquare)
+            if (moveDiagnostics['isValid'])
+                validMoves.push(nextSquare)
+            if (moveDiagnostics['break'])
+                break
+        }
+
+        return validMoves
     }
 }
