f9069d19d7de08ce6be21803a0aca237f5c0fbef
sophieclaxton <sophie.claxton@softwire.com>
2024-07-04 15:04:59 +0100

    Red-7 Implementation - kings can move

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -8,6 +8,22 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let validMoves = new Array(0)
+
+        let changes = [1, -1, 0]
+        let currentSquare = board.findPiece(this)
+
+        for (let rowChange of changes) {
+            for (let colChange of changes) {
+                if (rowChange == 0 && colChange == 0) {
+                    continue;
+                }
+                let nextSquare = currentSquare.moveBy(rowChange, colChange)
+                if (board.isOnBoard(nextSquare) && this.isValidMove(board, nextSquare)['isValid'])
+                    validMoves.push(nextSquare)
+            }
+        }
+
+        return validMoves
     }
 }
