04b06b7575a6045244112cd8128419a843561560
sophieclaxton <sophie.claxton@softwire.com>
2024-07-04 15:32:27 +0100

    Red-18 implementation - pawns taking on the diagonal

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -13,18 +13,23 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         let validMoves = new Array(0);
         let currentSquare = board.findPiece(this)
+
         let nextSquare1 : Square;
         let nextSquare2 : Square | null = null
+        let nextSquareDiags : Array<Square>
+
         switch (this.player) {
             case Player.WHITE:
                 nextSquare1 = currentSquare.moveBy(1, 0)
                 if (currentSquare.row == 1)
                     nextSquare2 = currentSquare.moveBy(2, 0)
+                nextSquareDiags = [currentSquare.moveBy(1, -1), currentSquare.moveBy(1, 1)]
                 break
             case Player.BLACK:
                 nextSquare1 = currentSquare.moveBy(-1, 0)
                 if (currentSquare.row == 6)
                     nextSquare2 = currentSquare.moveBy(-2, 0)
+                nextSquareDiags = [currentSquare.moveBy(-1, 1), currentSquare.moveBy(-1, -1)]
                 break
         }
         if (board.isOnBoard(nextSquare1) && board.getPiece(nextSquare1) === undefined)
@@ -34,6 +39,14 @@ export default class Pawn extends Piece {
                 validMoves.push(nextSquare2)
             }
         }
+
+        for (let diagMove of nextSquareDiags) {
+            if (board.isOnBoard(diagMove)) {
+                let pieceOnBoard = board.getPiece(diagMove)
+                if (pieceOnBoard !== undefined && pieceOnBoard.player != this.player && !pieceOnBoard.isKing) 
+                    validMoves.push(diagMove)
+            }
+        } 
         return validMoves;
     }
 }
