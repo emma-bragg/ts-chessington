a44c9f185571dc2f5b161081e98f883f2104cb94
sophieclaxton <sophie.claxton@softwire.com>
2024-07-04 14:00:47 +0100

    Red-1 implementation - pawn moves up and down one square

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,21 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let validMoves = new Array(0);
+        let currentSquare = board.findPiece(this)
+        let nextSquare : Square;
+        switch (this.player) {
+            case Player.WHITE:
+                nextSquare = Square.at(currentSquare.row + 1, currentSquare.col)
+                break
+            case Player.BLACK:
+                nextSquare = Square.at(currentSquare.row - 1, currentSquare.col)
+                break
+        }
+        if (nextSquare.row < GameSettings.BOARD_SIZE && nextSquare.row >= 0 && board.getPiece(nextSquare) === undefined)
+            validMoves.push(nextSquare)
+
+
+        return validMoves;
     }
 }
