6daf5bf5a6f98a5a87f48a1c2d94693d59f81f37
C0rMon <Colm.Monahan@softwire.com>
2024-07-04 16:38:30 +0100

    Added available moves for knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,31 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        const moveList = [];
+        let currentPosition = board.findPiece(this);
+        const diffMovelist = [
+            [2,1],
+            [2,-1],
+            [1,2],
+            [1,-2],
+            [-2,1],
+            [-2,-1],
+            [-1,2],
+            [-1,-2]
+        ];
+
+        for (let i = 0; i < diffMovelist.length; i++ ) {
+            let newMove = Square.at(
+                diffMovelist[i][0] + currentPosition.row,
+                diffMovelist[i][1] + currentPosition.col
+            );
+            
+            moveList.push(newMove);
+        }
+        return moveList; 
+
     }
+
+
 }
