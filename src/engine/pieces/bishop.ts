4b147b6f5b6bcf414a51f1f89562670bdf31aa77
GuyDav <guy.david@softwire.com>
2024-07-04 17:24:28 +0100

    refactored: bishops block moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -11,38 +11,25 @@ export default class Bishop extends Piece {
     public getAvailableMoves(board: Board) {
         let availableMoves = new Array<Square>();
         let currentPosition = board.findPiece(this);
-        let oldRow = currentPosition.row;
-        let oldCol = currentPosition.col;
         
-        for (let step = 0; step < Piece.boardSize; step++) {
-            let newRow = oldRow - step;
-            let newCol = oldCol - step;
-            if(this.addAvailableMove(availableMoves, newRow, newCol, currentPosition) && board.getPiece(new Square(newRow, newCol))){
-                break;
-            }
-        }
-        for (let step = 0; step < Piece.boardSize; step++) {
-            let newRow = oldRow - step;
-            let newCol = oldCol + step;
-            if(this.addAvailableMove(availableMoves, newRow, newCol, currentPosition) && board.getPiece(new Square(newRow, newCol))){
-                break;
-            }
-        }
-        for (let step = 0; step < Piece.boardSize; step++) {
-            let newRow = oldRow + step;
-            let newCol = oldCol - step;
-            if(this.addAvailableMove(availableMoves, newRow, newCol, currentPosition) && board.getPiece(new Square(newRow, newCol))){
-                break;
-            }
-        }
-        for (let step = 0; step < Piece.boardSize; step++) {
-            let newRow = oldRow + step;
-            let newCol = oldCol + step;
-            if(this.addAvailableMove(availableMoves, newRow, newCol, currentPosition) && board.getPiece(new Square(newRow, newCol))){
-                break;
-            }
-        }
+        this.quadrantCheck(-1,-1,availableMoves,currentPosition,board);
+        this.quadrantCheck(-1,1,availableMoves,currentPosition,board);
+        this.quadrantCheck(1,-1,availableMoves,currentPosition,board);
+        this.quadrantCheck(1,1,availableMoves,currentPosition,board);
         
         return availableMoves;
+    }
+
+    public quadrantCheck(rowStep : number, colStep : number, availableMoves : Array<Square>, currentPosition : Square, board : Board){
+        let oldRow = currentPosition.row;
+        let oldCol = currentPosition.col;
+        for (let step = 0; step < Piece.boardSize; step++) {
+            let newRow = oldRow + step * rowStep;
+            let newCol = oldCol + step * colStep;
+            if(this.addAvailableMove(availableMoves, newRow, newCol, currentPosition) && board.getPiece(new Square(newRow, newCol))){
+                break;
+            }
+        }
+
     }
 }
