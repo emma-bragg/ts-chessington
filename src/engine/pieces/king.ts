27ad54b8851e84d88588a76d6180cb2d834de4a4
GuyDav <guy.david@softwire.com>
2024-07-04 15:13:48 +0100

    Impl king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves = [];
+        let currentPosition = board.findPiece(this);
+        let oldRow = currentPosition.row;
+        let oldCol = currentPosition.col;
+
+        for (let index1 = -1; index1 < 2; index1++){
+            for (let index2 = -1; index2 < 2; index2++){
+                let newPos = new Square(oldRow+index1, oldCol+index2);
+                if (!newPos.equals(currentPosition) && newPos.inBoundsCheck()){
+                    availableMoves.push(newPos);
+                }
+            }
+        }
+
+        return availableMoves;
     }
 }
