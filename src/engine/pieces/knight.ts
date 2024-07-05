36bde43082110282e816a03d627b7f3bf5db4c2e
GuyDav <guy.david@softwire.com>
2024-07-05 14:27:06 +0100

    Refactored: knight page - removed comment + changed variable names

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -10,17 +10,16 @@ export default class Knight extends Piece {
 
     public getAvailableMoves(board: Board) {
         let availableMoves = new Array<Square>();
-        // let finalAvailableMoves = [];
         let currentPosition = board.findPiece(this);
         let oldRow = currentPosition.row;
         let oldCol = currentPosition.col;
 
-        let ones = [-1, 1]
-        let twos = [-2, 2]
-        for (let index1 = 0; index1 < ones.length; index1++) {
-            for (let index2 = 0; index2 < twos.length; index2++) {
-                this.addAvailableMoveAndContinue(availableMoves, oldRow+ones[index1], oldCol+twos[index2], currentPosition, board);
-                this.addAvailableMoveAndContinue(availableMoves, oldRow+twos[index2], oldCol+ones[index1], currentPosition, board);
+        let shortSteps = [-1, 1]
+        let longSteps = [-2, 2]
+        for (let index1 = 0; index1 < shortSteps.length; index1++) {
+            for (let index2 = 0; index2 < longSteps.length; index2++) {
+                this.addAvailableMoveAndContinue(availableMoves, oldRow+shortSteps[index1], oldCol+longSteps[index2], currentPosition, board);
+                this.addAvailableMoveAndContinue(availableMoves, oldRow+longSteps[index2], oldCol+shortSteps[index1], currentPosition, board);
             }
         }
 
